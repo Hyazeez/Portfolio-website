f486cab3dc91ded618a866f1cdf25bd5a02dfb8a
Azeez shaban <hyazeez123@gmail.com>
2025-06-26 03:42:19 +0530

    Update portfolio components

---
+++ assets/assets.js
@@ -0,0 +1,85 @@
+import android from '../assets/android.png';
+import angular from '../assets/angular.png';
+import arrow from '../assets/arrow.png';
+import aws from '../assets/aws.png';
+import canva from '../assets/canva.png';
+import capcut from '../assets/capcut.png';
+import close from '../assets/close.png';
+import code from '../assets/code.png';
+import css from '../assets/css.png';
+import docker from '../assets/docker.png';
+import download from '../assets/download.png';
+import education from '../assets/education.png';
+import expressjs from '../assets/expressjs.png';
+import firebase from '../assets/firebase.png';
+import git from '../assets/git.png';
+import googlecloud from '../assets/googlecloud.png';
+import graphics from '../assets/graphics.png';
+import html from '../assets/html.png';
+import intellijidea from '../assets/intellijidea.png';
+import java from '../assets/java.png';
+import js from '../assets/js.png';
+import logo from '../assets/logo.png';
+import mail from '../assets/mail.png';
+import menu from '../assets/menu.png';
+import mongodb from '../assets/mongodb.png';
+import moon from '../assets/moon.png';
+import mysql from '../assets/mysql.png';
+import nextjs from '../assets/nextjs.png';
+import nodejs from '../assets/nodejs.png';
+import powerbi from '../assets/powerbi.png';
+import project from '../assets/project.png';
+import python from '../assets/python.png';
+import react from '../assets/react.png';
+import rightarrow from '../assets/rightarrow.png';
+import send from '../assets/send.png';
+import springboot from '../assets/springboot.png';
+import sun from '../assets/sun.png';
+import tailwind from '../assets/tailwind.png';
+import upperarrow from '../assets/upperarrow.png';
+import userimg from '../assets/user-img.jpeg';
+import website from '../assets/website.png';
+
+export const assets = {
+    android,
+    angular,
+    arrow,
+    aws,
+    canva,
+    capcut,
+    close,
+    code,
+    css,
+    docker,
+    download,
+    education,
+    expressjs,
+    firebase,
+    git,
+    googlecloud,
+    graphics,
+    html,
+    intellijidea,
+    java,
+    js,
+    logo,
+    mail,
+    menu,
+    mongodb,
+    moon,
+    mysql,
+    nextjs,
+    nodejs,
+    powerbi,
+    project,
+    python,
+    react,
+    rightarrow,
+    send,
+    springboot,
+    sun,
+    tailwind,
+    upperarrow,
+    userimg,
+    website
+};
